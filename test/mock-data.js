f85f39f8c81afb2c9edf53238f8e9fd7e25061de
Eric Batiste <ericbatiste4@gmail.com>
2024-01-11 22:18:16 -0700

    Create mock data sets for testing.

---
+++ test/mock-data.js
@@ -0,0 +1,98 @@
+export const bookings = [
+  {
+    id: 'book1',
+    userID: 9,
+    date: '2022/04/22',
+    roomNumber: 15
+  },
+  {
+    id: 'book2',
+    userID: 43,
+    date: '2022/01/24',
+    roomNumber: 24
+  },
+  {
+    id: 'book3',
+    userID: 13,
+    date: '2022/01/10',
+    roomNumber: 12
+  },
+  {
+    id: 'book4',
+    userID: 20,
+    date: '2022/02/16',
+    roomNumber: 7
+  },
+  {
+    id: 'book5',
+    userID: 1,
+    date: '2022/02/05',
+    roomNumber: 12
+  }
+];
+
+export const rooms = [
+  {
+    number: 1,
+    roomType: 'residential suite',
+    bidet: true,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 358.4
+  },
+  {
+    number: 2,
+    roomType: 'suite',
+    bidet: false,
+    bedSize: 'full',
+    numBeds: 2,
+    costPerNight: 477.38
+  },
+  {
+    number: 3,
+    roomType: 'single room',
+    bidet: false,
+    bedSize: 'king',
+    numBeds: 1,
+    costPerNight: 491.14
+  },
+  {
+    number: 4,
+    roomType: 'single room',
+    bidet: false,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 429.44
+  },
+  {
+    number: 5,
+    roomType: 'junior suite',
+    bidet: true,
+    bedSize: 'queen',
+    numBeds: 1,
+    costPerNight: 397.02
+  }
+];
+
+export const customers = [
+  {
+    id: 1,
+    name: 'Leatha Ullrich'
+  },
+  {
+    id: 2,
+    name: 'Rocio Schuster'
+  },
+  {
+    id: 3,
+    name: 'Kelvin Schiller'
+  },
+  {
+    id: 4,
+    name: 'Kennedi Emard'
+  },
+  {
+    id: 5,
+    name: 'Rhiannon Little'
+  }
+];
--- test/users-test.js
+++ test/users-test.js
@@ -1,8 +1,10 @@
 import chai from 'chai';
 const expect = chai.expect;
+import { bookings, rooms, customers } from './mock-data.js'
 
 describe('See if the tests are running', function() {
   it('should return true', function() {
     expect(true).to.equal(true);
   });
 });
+
