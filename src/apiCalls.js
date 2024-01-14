18ba7aa3497c673c42ca6aae7d216713002a1069
Eric Batiste <ericbatiste4@gmail.com>
2024-01-13 22:17:28 -0700

    Build out basic network requests in apiCalls.js.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,3 +11,73 @@ const dataUrls = [
   'http://localhost:3001/api/v1/bookings'
 ];
 
+export const getAllData = () => {
+  Promise.all(dataUrls.map(url => {
+    fetch(url).then(response => {
+      if (!response.ok) {
+        throw new Error('Try harder you fool!')
+      }
+      return response.json();
+    })
+    .then(data => {
+      // customers = data.customers;
+      // rooms = data.rooms;
+      // bookings = data.bookings;
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }))
+}
+
+export const getUser = (id) => {
+  fetch(`http://localhost:3001/api/v1/customers/${id}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Try harder you fool!')
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+export const postBooking = () => {  
+  fetch('http://localhost:3001/api/v1/bookings', {
+    method: 'POST',
+    body: JSON.stringify(someDataToSend),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Woops!')
+    }
+    return response.json()
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+}
+
+export const deleteBooking = (id) => {  
+  fetch(`http://localhost:3001/api/v1/bookings/${id}`, {
+    method: 'POST',
+    body: JSON.stringify(someDataToSend),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Woops!')
+    }
+    return response.json()
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,5 @@
+import { getAllData, getUser } from './apiCalls.js';
+
 import {
   calcTotalCost,
   compileUserBookings,
@@ -6,11 +8,13 @@ import {
   filterRoomsByType,
   setCurrentUser,
   userSelectRoom
-} from '../users.js';
+} from './users.js';
+
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import '../images/james-donaldson-beach.png';
+import './images/turing-logo.png';
 
+getAllData()
